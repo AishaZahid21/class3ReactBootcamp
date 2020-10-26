1688877e5218d3bcb9fe2a902a38fa902b016d69
Ayesha Zahid <47062178+AishaZahid21@users.noreply.github.com>
2020-10-27 00:40:01 +0500

    update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 import Todo from "./todo.js"
 
-var tasks=["Bootcamp class","Virtual Assistant class"]
+var tasks=["Bootcamp class","Virtual Assistant class", "blahhhh"]
 
 function App() {
   
